eb6f5d0527aa5f921ebe83d92e3abec2d9167708
jorgeantonio21 <matroid@outlook.com>
2023-12-10 17:00:18 +0000

    updates to cargo.toml file

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,5 +1,3 @@
-pub mod client;
-
 pub mod client_auth {
     tonic::include_proto!("zkp_auth");
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,71 @@
-use client::client_auth::{auth_client::AuthClient, RegisterRequest};
+use client::client_auth::{
+    auth_client::AuthClient, AuthenticationChallengeRequest, RegisterRequest,
+};
 use log::info;
+
+use clap::{Parser, Subcommand};
+use tonic::Request;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    // user registration
+    Register {
+        // user name
+        #[arg(short, long)]
+        name: String,
+        // user password
+        #[arg(short, long)]
+        password: String,
+    },
+    // user authentication
+    Login {
+        // user name
+        #[arg(short, long)]
+        name: String,
+        // user password
+        #[arg(short, long)]
+        password: String,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
+    let cli = Cli::parse();
+
+    info!("Connecting to server... ");
     let mut client = AuthClient::connect("https://localhost:5001").await?;
 
-    let request = tonic::Request::new(RegisterRequest {
-        user: String::from("I"),
-        y1: 0,
-        y2: 1,
-    });
+    match cli.command {
+        Commands::Register { name, password } => {
+            info!("Registering user with name: {name} ...");
+            client
+                .register(Request::new(RegisterRequest {
+                    user: name,
+                    y1: 0,
+                    y2: 1,
+                }))
+                .await?;
+        }
+        Commands::Login { name, password } => {
+            info!("User {name} logging in ...");
 
-    let response = client.register(request).await?;
-
-    info!(
-        "Request successfully processed with response: {:?}",
-        response
-    );
+            client
+                .create_authentication_challenge(Request::new(AuthenticationChallengeRequest {
+                    user: name,
+                    r1: 0,
+                    r2: 1,
+                }))
+                .await?;
+        }
+    }
 
     Ok(())
 }
